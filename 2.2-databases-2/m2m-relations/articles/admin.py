28006e3a5be602ccdfea801caf8dac3250260e62
Anton Selivanov <freelanset@gmail.com>
2023-06-10 14:15:15 +0800

    Homework 2.2 done

--- 2.2-databases-2/m2m-relations/articles/admin.py
+++ 2.2-databases-2/m2m-relations/articles/admin.py
@@ -8,16 +8,18 @@ from .models import Article, Tag, Scope
 class ScopeInlineFormset(BaseInlineFormSet):
     def clean(self):
         count = 0
+        tags = []
         for form in self.forms:
-            try:
+            if form.cleaned_data:
+                tags.append(form.cleaned_data['tag'])
                 if form.cleaned_data['is_main']:
                     count += 1
-            except KeyError:
-                pass
         if count == 0:
             raise ValidationError('Укажите основной раздел')
         if count > 1:
             raise ValidationError('Основным может быть только один раздел')
+        if len(tags) != len(set(tags)):
+            raise ValidationError('Один из разделов выбран несколько раз')
         return super().clean()
 
 
